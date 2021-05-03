2bec16c2814c05fb79cbe616e62dc97161db37ef
O-Sue <68745170+O-Sue@users.noreply.github.com>
2021-05-03 21:52:33 +0200

    Update tracés transect diverses prof.py

--- tracés transect diverses prof.py
+++ tracés transect diverses prof.py
@@ -21,8 +21,8 @@ for p in transchoice:
     #IMEDIA = xr.open_dataset("courants_IMEDIA_TIE120" + str(p) +"skaphi_14032012_reduc.nc", decode_times=True)
     
     IMEDIA = xr.open_dataset("courants_IMEDIA_TIE120" + str(p) + "sk_15032012_new_complete.nc", decode_times=True)
-    lat=IMEDIA['LAT'] #43.0475 to 43.366825
-    lon=IMEDIA['LON'] #6.022179 to 7.188118
+    lat=IMEDIA['LAT'] 
+    lon=IMEDIA['LON'] 
     lat= np.array(lat.data)
     lon= np.array(lon.data)
     u=IMEDIA['U'][:,10]
@@ -42,33 +42,26 @@ for p in transchoice:
         
     mymap=Basemap(projection='merc',llcrnrlat=43.2,urcrnrlat=44.3,llcrnrlon=8.2,urcrnrlon=10.3,resolution='h')
         
-    X, Y = mymap(lon, lat)    # Basemap ne reconnait pas lon lat directement donc conversion x/y
+    X, Y = mymap(lon, lat)    
     mymap.drawcoastlines()
-    myparallels=np.arange(-90,90+1,1)    #  le dernier 1 affiche tout les 1 lon/lat en légende/quadrillage
+    myparallels=np.arange(-90,90+1,1)    
     mymeridians = np.arange(-180,180+1,1) 
     mymap.drawparallels(myparallels,labels=[1,0,0,0],fontsize=35)  
     mymap.drawmeridians(mymeridians,labels=[0,0,0,1],fontsize=35)
     mymap.fillcontinents(color='0.83',lake_color='0.83',zorder=100)
     
 
-
-#mymap.plot(X,Y , '+')
-    Resolution=np.hypot(u, v)
-    mymap.quiver(X,Y,u,v, Resolution, width=0.0050, scale=3) #scale 10cbié 7 plus long 5 ils commencent la superposition
-    plt.rcParams['xtick.labelsize'] = 30
-    plt.rcParams['ytick.labelsize'] = 30                        # augmente la taille de ticks de la colorbar 
-    plt.colorbar(aspect=30, orientation = 'horizontal')
+    #mymap.plot(X,Y , '+')
     
+    Resolution=np.hypot(u, v)
+    mymap.quiver(X,Y,u,v, Resolution, width=0.0050, scale=3) 
+    plt.rcParams['xtick.labelsize'] = 30
+    plt.rcParams['ytick.labelsize'] = 30                       
+    plt.colorbar(aspect=30, orientation = 'horizontal') 
     plt.clim(0,1.05)
-
     plt.figtext(0.5, 0.25, 'vitesse en m/s', size = 30)
     plt.ylabel("Lat", size = 30, position=(1,0.95))
-    plt.xlabel("Lon", size = 30, position=(1,1))
-                  
-                #plt.streamplot(X4, Y4, u[i], v[i])
-                #plt.streamplot(X, Y, u, v, color=np.sqrt(u**2+v**2), linewidth = 2, cmap =plt.cm.viridis)
-                
-                
+    plt.xlabel("Lon", size = 30, position=(1,1))   
     plt.title("Courant horizontaux le long de la trajectoire de la campagne IMEDIA à la profondeur 50.5 m", position = (0.5, 1.1 ), size = 35)
     plt.savefig("50,5m TIE120" + str(p)+".png")
 
